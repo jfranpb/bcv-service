488e67520440fce9e3a981f2d1dace8509b343d9
jfranpb <jfranpb@gmail.com>
2025-12-13 12:19:35 -0400

    Update bcv.js

--- api/bcv.js
+++ api/bcv.js
@@ -1,14 +1,17 @@
 export default async function handler(req, res) {
   try {
-    // Cambia TU_DOMINIO por tu dominio real de Vercel
-    const response = await fetch("https://TU_DOMINIO.vercel.app/bcv.json");
+    const response = await fetch(
+      "https://bcv-service-qtt7uxmwf-jose-francisco-perez-bernals-projects.vercel.app/bcv.json"
+    );
 
     const data = await response.json();
 
     const rawRate = data.USD; // Ej: "36,45"
 
     if (!rawRate) {
-      return res.status(500).json({ error: "No se pudo obtener la tasa desde bcv.json" });
+      return res.status(500).json({
+        error: "No se pudo obtener la tasa desde bcv.json"
+      });
     }
 
     const rate = parseFloat(rawRate.replace(",", "."));
